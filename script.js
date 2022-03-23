3f661f5035ed7c43d9088e725fc045d28766397f
kluci-hpbook <kluci-hpbook@localhost.localdomain>
2022-03-23 17:48:21 +0100

    vyrobení random generátoru vět

--- script.js
+++ script.js
@@ -1,31 +1,48 @@
 const zelenina = [
-    'rajče',
-    'okurka',
-    'paprika',
-    'mrkev',
-    'brambory',
-    'celer',
-    'zelí',
-    'květák',
-    'ředkvičky',
-    'brokolice'
-]
+  "rajče",
+  "okurka",
+  "paprika",
+  "mrkev",
+  "brambory",
+  "celer",
+  "zelí",
+  "květák",
+  "ředkvičky",
+  "brokolice",
+];
 
-const ovoce = [
-    'jablko',
-    'hruška',
-    'jahody',
-    'třešně'
-]
+const zelenina2 = ["rajče", "okurka", "paprika", ["bobek", "hovno"]];
+zelenina2.push("sračky");
+zelenina2.pop();
+console.log(zelenina2.length);
+const ovoce = ["jablko", "hruška", "jahody", "třešně"];
+const zdravy = zelenina.concat(ovoce);
+console.log(zdravy.join("ahoj "));
+zelenina.indexOf("okurka");
+
+console.log(ovoce.includes("jablko"));
+const cisla = [1, 23, 435, 5, 2];
+console.log(cisla.sort());
 
 const vety = [
-   "Zůstaň stát",
-   "Přeskoč o dvě pole dopředu",
-   "Dej se doprava",
-   "Dej se doleva",
-   "Jdi rovně",
-   "Jedno kolo stojíš" 
-]
+  "Zůstaň stát",
+  "Přeskoč o dvě pole dopředu",
+  "Dej se doprava",
+  "Dej se doleva",
+  "Jdi rovně",
+  "Jedno kolo stojíš",
+];
+
+const kostka= document.querySelector("h2");
+const tlacitko= document.querySelector("button");
+
+tlacitko.onclick = () => {
+    const nahodne_cislo = Math.floor(Math.random() * 6);
+    kostka.textContent = vety[nahodne_cislo];
+};
+
+console.log(vety[5]);
+
 
 
 // index
@@ -37,6 +54,3 @@ const vety = [
 // includes
 // sort
 // Math.random()
-
-
-
